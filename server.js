d7115d2f39a72f03a94506c1a4b294be3428fb4d
Rodrigo Lessa <rodrigo.lessa@ghap.com.br>
2022-11-07 23:07:00 -0300

    ATUALIZANDO API E HOSPENDANDO NO RAILWAY

--- server.js
+++ server.js
@@ -36,11 +36,21 @@ app.listen(port, () => {
 
 const Pool = require("pg").Pool;
 const pool = new Pool({
+  
+  /*
   user: "postgres",
   host: "localhost",
   database: "pulsar",
   password: "pulsar",
   port: 5432,
+  */
+
+  user: "postgres",
+  host: "containers-us-west-113.railway.app",
+  database: "railway",
+  password: "lLZKfPKkseKKt9y3Hht1",
+  port: 5581,
+
 });
 
 // ENDPOINTS //
